17f0637e3b39ff2b92e12b0443cf48ccda3f6662
Red <crabmarket56@gmail.com>
2022-07-12 11:56:04 -0400

    Add dictionary/array

---
+++ script.rb
@@ -0,0 +1,7 @@
+
+def substrings(word, dictionary)
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary_array = dictionary.split(',')
+
+
+end
