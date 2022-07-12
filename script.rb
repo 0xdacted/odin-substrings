3693d25dfb9124897b92f6b5fa0b77aa5fb6b502
Red <crabmarket56@gmail.com>
2022-07-12 14:40:18 -0400

    Determines whether the string can be found in the array of substrings

--- script.rb
+++ script.rb
@@ -1,7 +1,16 @@
+# takes input string and an array of substrings, determines whether the input string is included in the array of substrings, 
+#  returns a hash listing each substring that was found in the input string and how many times it was found
+require 'pry-byebug'
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word, dictionary)
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-dictionary_array = dictionary.split(',')
-
-
+hash = {}
+times = 0
+if dictionary.include?(word)
+  times +=1
+  hash = {substring: word, times: times}
 end
+hash
+end
+
+substrings("below", dictionary)
