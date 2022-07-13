a968e14cfbb32004233dbd4137f2d1f6c34386b9
Red <crabmarket56@gmail.com>
2022-07-13 08:04:35 -0400

    remove debugging

--- script.rb
+++ script.rb
@@ -1,6 +1,5 @@
 # takes input string and an array of substrings, determines whether any substring was found in the input string , 
-#  returns a hash listing each substring that was found in the input string and how many times it was found
-require 'pry-byebug'
+#  returns a hash listing each substring that was found in the input string and a counter of times it was found
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
